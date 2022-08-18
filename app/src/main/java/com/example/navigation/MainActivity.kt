3b20ea5e7c6f4d70a668926a6d8282580d6cfccd
Soumyadeep Mondal <soumyadeepmondal@Soumyadeeps-MacBook-Air.local>
2022-08-18 08:42:12 +0530

    back button

--- app/src/main/java/com/example/navigation/MainActivity.kt
+++ app/src/main/java/com/example/navigation/MainActivity.kt
@@ -3,12 +3,25 @@ package com.example.navigation
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
 import com.example.navigation.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
+        val binding =
+            DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
+
+        val navController = this.findNavController(R.id.titleFragment)
+        NavigationUI.setupActionBarWithNavController(this, navController)
+
+
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = this.findNavController(R.id.titleFragment)
+        return navController.navigateUp()
     }
 }
